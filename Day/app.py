a6c25cfa4f122c16596286b037c7b7e0ccea04f7
Ryan Curt <rclassit@ncsu.edu>
2021-05-16 19:11:31 -0400

    updating app.py file

--- Day/app.py
+++ Day/app.py
@@ -3,16 +3,18 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import requests
 import json
+from sklearn import *
+
 
 app = Flask(__name__)
-model=None
-#model = pickle.load(open('pickle_rf.pkl', 'rb'))
 
-#sample data
-# data = [4,2,3,4,3,6]
-# prediction = model.predict(np.array([data]))
+model = pickle.load(open('pickle_rf.pkl', 'rb'))
+
+# sample data
+data = [4,2,3,4,3,6]
+prediction = model.predict(np.array([data]))
     
-#print(prediction)
+print(prediction)
 
 #route for home
 @app.route('/')
@@ -22,19 +24,21 @@ def home():
 
 @app.route('/results',methods=['POST'])
 def results():
-    
-    # data = request.get_json(force=True)
-    # prediction = model.predict([np.array(data)])
+    response_dict=json.loads(request.data)
+    int_features = [int(x) for x in request.data]
+    final_features = [np.array(int_features)]
+    prediction= model.predict(final_features)
     print(request.data)
     
-    response_dict=json.loads(request.data)
+    
 
-    #below response_dict is input for model
+    # below response_dict is input for model
     print(response_dict["overall"])
 
-    #eventually put model output below (in jsonify)
+    
+    # eventually put model output below (in jsonify)
     return jsonify("successfully connected to web api")
-
+    return render_template('index.html', prediction_text='Sales should be $ {}'.format(prediction))
 
 if __name__ == "__main__":
     app.run(debug=True)

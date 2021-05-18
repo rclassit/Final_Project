6b35f4a98b096ee1b9e257733f23eee34e4a3752
Lindsay <72901101+lindsaykh@users.noreply.github.com>
2021-05-17 21:43:39 -0400

    most recent

--- Day/app.py
+++ Day/app.py
@@ -1,8 +1,10 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect
 import pickle
 import requests
 import json
+import sqlalchemy as db
+#from json import JSONEncoder
 #from sqlalchemy import create_engine, func
 #import psycopg2
 #from flask_cors import CORS, cross_origin
@@ -22,12 +24,14 @@ def home():
     return render_template('index.html')
 
 #route to send input to model
-@app.route('/results',methods=['GET', 'POST'])
+@app.route('/results',methods=['GET','POST'])
 def results():
    
     #below input is from d3/js (json stringify-ed)
     input = request.data
     print(f'this is the input from the dropdown: {input}')
+    data = [input[0],input[1],input[2],input[3],input[4],input[5]]
+    print(f'this is what data looks like with index in input: {data}')
 
     #un stringify-ed to put in model
     model_input = json.loads(input)
@@ -36,25 +40,27 @@ def results():
     #prediction results hot encoded
     prediction = model.predict(np.array([model_input]))
     print(f'this is the hot encoded prediction: {prediction}')
+         
 
     #go to database to get actual value
-    # rds_connection_string = "root:WE0ew4F3H2U0@finalproject.cqyw28rarsf4.us-east-1.rds.amazonaws.com:5432/postgres"
-    # engine = create_engine(f'postgresql://{rds_connection_string}')
-    # connection = engine.connect()
-    # metadata = db.MetaData()
-    # beer_style = db.Table('beer_style', metadata, autoload=True, autoload_with=engine)
-    # predlist = np.where(prediction == 1.0)
-    # id = int(predlist[1])
-    # query = db.select([beer_style.columns.beer_style]).where(beer_style.columns.beer_style_id == id)
-    # result = pd.DataFrame(connection.execute(query).fetchall())
-    # beer = result.iloc[0, 0]
-    #print(f'this is the prediction: {beer}')
+    rds_connection_string = "root:WE0ew4F3H2U0@finalproject.cqyw28rarsf4.us-east-1.rds.amazonaws.com:5432/postgres"
+    engine = db.create_engine(f'postgresql://{rds_connection_string}')
+    connection = engine.connect()
+    metadata = db.MetaData()
+    beer_style = db.Table('beer_style', metadata, autoload=True, autoload_with=engine)
+    predlist = np.where(prediction == 1.0)
+    id = int(predlist[1])
+    query = db.select([beer_style.columns.beer_style]).where(beer_style.columns.beer_style_id == id)
+    result = pd.DataFrame(connection.execute(query).fetchall())
+    beer = result.iloc[0, 0]
+    print(f'this is the prediction: {beer}')
     
-    prediction_list = list(prediction)
-    prediction_json = json.dumps(prediction_list)
-    print(prediction_json)
+    # GET request
+    if request.method == 'GET':
+        return render_template("index.html", beer=beer)  
     #post prediction result to js/html
-    return render_template('index.html', prediction=prediction_json)
+    #return redirect('/#result', code=302,beer=beer)
+    
 
 
 #route to put model results into js/html

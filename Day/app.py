db893c99645786b0dda3322cff3aaa30fedce845
Lindsay <72901101+lindsaykh@users.noreply.github.com>
2021-05-21 22:24:19 -0400

    added simple render template

--- Day/app.py
+++ Day/app.py
@@ -60,29 +60,10 @@ def results():
     beer = result.iloc[0, 0]
     print(f'this is the prediction: {beer}')
     
-    # GET request
-    if request.method == 'GET':
-        output = {'greeting':beer}
-        print(output)
-        return jsonify(output) #serialize and use JSON headers
-
-    #return render_template("index.html", beer=beer)  
-    #post prediction result to js/html
-    #return redirect('/#result', code=302,beer=beer)
+    #result correctly prints in terminal but does not print to html (render_template below doesn't work)
+    return render_template('index.html',value=beer)
     
 
-
-#route to put model results into js/html
-# @app.route('/test', methods=['GET', 'POST'])
-# def testfn():
-#     # GET request
-#     if request.method == 'GET':
-#         message = {'greeting':'Hello from Flask!'}
-#         return jsonify(message)  # serialize and use JSON headers
-#     # POST request
-#     if request.method == 'POST':
-#         print(request.get_json())  # parse as JSON
-#         return 'Sucesss', 200
 
 if __name__ == "__main__":
     app.run(debug=True)
